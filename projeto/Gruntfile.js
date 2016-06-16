d63021e49aff30eb383b42ab916c9a97c713b45f
mimica <jmimica@gmail.com>
2016-06-16 20:45:40 -0300

    Update chapter 3.

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -23,16 +23,24 @@ module.exports = function(grunt) {
 
 		usemin: {
 			html: 'dist/**/*.html'
-		}
+		},
 
-
+		imagemin: {
+	    	public: {
+	        	expand: true,
+	        	cwd: 'public/img',
+	        	src: '**/*.{png,jpg,gif}',
+	        	dest: 'dist/img'
+	      	}
+	    }
 
 	});
 
 	// registrando task para minificação
 	grunt.registerTask('dist', ['clean', 'copy']);
 
-	grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin']);
+	grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 
+		'usemin', 'imagemin']);
 
 	// registrando tasks
 	grunt.registerTask('default', ['dist', 'minifica']);
@@ -44,4 +52,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 }
